3cc7977ee083726f5802a714a8c8a180aafbe4bf
Ashish <ashish@cropsly.com>
2023-12-23 11:30:54 +0530

    dark/light mode setup

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Open_Sans } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
+import { ThemeProvider } from "@/components/providers/theme-providers";
 
 const font = Open_Sans({ subsets: ["latin"] });
 
@@ -17,8 +18,10 @@ export default function RootLayout({
 }) {
   return (
     <ClerkProvider>
-      <html lang="en">
-        <body className={font.className}>{children}</body>
+      <html lang="en" suppressHydrationWarning>
+        <body className={font.className}>
+          <ThemeProvider attribute="class" defaultTheme="dark" enableSystem={false} storageKey="discord-theme">{children}</ThemeProvider>
+        </body>
       </html>
     </ClerkProvider>
   );
