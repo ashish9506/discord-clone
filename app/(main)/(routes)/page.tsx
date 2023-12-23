a5159acf5f3a95ebc434ddf0067a98f6485db918
Ashish <ashish@cropsly.com>
2023-12-23 11:14:22 +0530

    basic auth setup

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,10 +1,10 @@
 import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
     <div>
-      <p className="text-3xl font-bold text-indigo-500">Hello</p>;
-      <Button>Click Me</Button>
+      <UserButton afterSignOutUrl="/" />
     </div>
   );
 }
