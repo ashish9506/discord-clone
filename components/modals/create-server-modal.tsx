7c15d93b1e63dd40513dd8da137360ca231229a0
Ashish <ashish@cropsly.com>
2023-12-30 11:38:30 +0530

    create server modal

--- components/modals/create-server-modal.tsx
+++ components/modals/create-server-modal.tsx
@@ -24,9 +24,9 @@ import {
 
 import { Input } from "../ui/input";
 import { Button } from "../ui/button";
-import { useEffect, useState } from "react";
 import { FileUpload } from "../file-upload";
 import { useRouter } from "next/navigation";
+import { useModal } from "@/hooks/use-modal-store";
 
 const formSchema = z.object({
   name: z.string().min(1, {
@@ -37,13 +37,11 @@ const formSchema = z.object({
   }),
 });
 
-export const InitialModal = () => {
-  const [isMounted, setIsMounted] = useState(false);
+export const CreateServerModal = () => {
+  const { isOpen, onClose, type } = useModal();
+  const router = useRouter();
 
-  const router  = useRouter()
-  useEffect(() => {
-    setIsMounted(true);
-  }, []);
+  const isModalOpen = isOpen && type === "createServer";
 
   const form = useForm({
     resolver: zodResolver(formSchema),
@@ -62,17 +60,20 @@ export const InitialModal = () => {
         data: values,
       });
       form.reset();
-      router.refresh()
-      window.location.reload()
+      router.refresh();
+      onClose();
     } catch (error) {
       console.log(error);
     }
   };
 
-  if (!isMounted) return null;
+  const handleClose = () => {
+    form.reset();
+    onClose();
+  };
 
   return (
-    <Dialog open>
+    <Dialog open={isModalOpen} onOpenChange={handleClose}>
       <DialogContent className="bg-white text-black p-0 overflow-hidden">
         <DialogHeader className="pt-8 px-6">
           <DialogTitle className="text-2xl text-center font-bold">
--- components/navigation/navigation-item.tsx
+++ components/navigation/navigation-item.tsx
@@ -20,7 +20,7 @@ export const NavigationItem = ({ id, imageUrl, name }: NavigationItemProps) => {
   };
   return (
     <ActionTooltip side="right" align="center" label={name}>
-      <button onClick={() => {}} className="group relative flex items-center">
+      <button onClick={onClick} className="group relative flex items-center">
         <div
           className={cn(
             "absolute left-0 bg-primary rounded-r-full transition-all w-[4px]",
