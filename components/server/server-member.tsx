28dfa44940b2bbea2db7a634588abdbb580928c2
Ashish <ashish@cropsly.com>
2024-01-11 13:53:19 +0530

    showing channels and members in sidebar

---
+++ components/server/server-member.tsx
@@ -0,0 +1,45 @@
+"use client";
+
+import { cn } from "@/lib/utils";
+import { Member, MemberRole, Profile, Server } from "@prisma/client";
+import { ShieldAlert, ShieldCheck } from "lucide-react";
+import { useParams } from "next/navigation";
+import { UserAvatar } from "../user-avatar";
+
+interface ServerMemberProps {
+  member: Member & { profile: Profile };
+  server: Server;
+}
+
+const roleIconMap = {
+  [MemberRole.ADMIN]: <ShieldAlert className="w-4 h-4 ml-2 text-rose-500" />,
+  [MemberRole.MODERATOR]: (
+    <ShieldCheck className="w-4 h-4 ml-2 text-indigo-500" />
+  ),
+  [MemberRole.GUEST]: null,
+};
+export const ServerMember = ({ server, member }: ServerMemberProps) => {
+  const icon = roleIconMap[member.role];
+
+  const params = useParams();
+  return (
+    <button
+      className={cn(
+        "group px-2 py-2 rounded-md flex items-center gap-x-2 w-full hover:bg-zinc-700/10 dark:hover:bg-zinc-700/50 transition mb-1",
+        params?.memberId === member.id && "bg-zinc-700/20 dark:bg-zinc-700"
+      )}
+    >
+      <UserAvatar src={member.profile.imageUrl} className="w-8 h-8" />
+      <p
+        className={cn(
+          "font-semibold text-sm text-zinc-500 group-hover:text-zinc-600 dark:text-zinc-400 dark:group-hover:text-zinc-300 transition",
+          params?.memberId === member.id &&
+            "text-primary dark:text-zinc-200 dark:group-hover:text-white"
+        )}
+      >
+        {member.profile.name}
+      </p>
+      {icon}
+    </button>
+  );
+};
